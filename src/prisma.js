76090657f6d404c3d0e7632adfcdd5f38f36736c
Aleksandr Nikitin <nikitinal.nal@gmail.com>
2019-04-02 12:43:45 +0300

    Extract prisma secret to env var

--- src/prisma.js
+++ src/prisma.js
@@ -2,8 +2,8 @@ import { Prisma } from 'prisma-binding';
 import  {fragmentReplacements} from './resolvers/index'
 const prisma = new Prisma({
   typeDefs: 'prisma/generated/prisma.graphql',
-  endpoint: process.env.PRISMA_ENDPOINT,
-  secret: 'supersercret4unknownpeople',
+  endpoint: process.env.PRISMA_SECRET, // http://192.168.99.100:4466
+  secret: process.env.PRISMA_SECRET,     // 'supersercret4unknownpeople'
   fragmentReplacements
 })
 
